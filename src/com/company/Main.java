385f25b04309ecdfcb3f673ec03b5b89f8ef582b
Dezyuk <romanyuk.groz@gmail.com>
2022-03-15 20:20:12 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Background
+        String RESET = "\u001B[0m";
+        String GRAY237 = "\u001b[48;5;237m";
+        String GRAY102 = "\u001b[48;5;102m";
+        String GRAY246 = "\u001b[48;5;246m";
+        String RED124 = "\u001b[48;5;124m";
+        String RED88 = "\u001b[48;5;88m";
+        String RED196 = "\u001b[48;5;196m";
+        String WHITE = "\u001b[48;5;231m";
+        System.out.println(WHITE+"                                                                                          "+RESET);
+        System.out.println(WHITE+"                                       "+GRAY237+"               "+WHITE+"                                    "+RESET);
+        System.out.println(WHITE+"                                 "+GRAY237+"                              "+WHITE+"                           "+RESET);
+        System.out.println(WHITE+"                              "+GRAY237+"      "+RED196+"                        "+GRAY237+"      "+WHITE+"                        "+RESET);
+        System.out.println(WHITE+"                              "+GRAY237+"   "+RED196+"                              "+GRAY237+"      "+WHITE+"                     "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"      "+RED196+"               "+GRAY237+"                     "+WHITE+"                     "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED196+"            "+GRAY237+"         "+GRAY102+"               "+GRAY237+"      "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED196+"         "+GRAY237+"      "+GRAY102+"   "+WHITE+"                     "+GRAY102+"   "+GRAY237+"   "+WHITE+"               "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED196+"         "+GRAY237+"      "+GRAY246+"      "+WHITE+"                  "+GRAY246+"   "+GRAY102+"   "+GRAY237+"   "+WHITE+"            "+RESET);
+        System.out.println(WHITE+"                  "+GRAY237+"            "+RED196+"         "+GRAY237+"   "+GRAY102+"      "+GRAY246+"                        "+GRAY102+"   "+GRAY237+"   "+WHITE+"            "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"      "+RED196+"   "+GRAY237+"      "+RED196+"         "+GRAY237+"      "+GRAY102+"      "+GRAY246+"                     "+GRAY102+"   "+GRAY237+"   "+WHITE+"            "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED196+"      "+GRAY237+"      "+RED196+"            "+GRAY237+"   "+GRAY102+"                           "+GRAY237+"      "+WHITE+"            "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED196+"      "+GRAY237+"      "+RED196+"            "+GRAY237+"      "+GRAY102+"                  "+GRAY237+"         "+WHITE+"               "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED196+"   "+GRAY237+"      "+RED124+"   "+RED196+"            "+GRAY237+"                           "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"      "+GRAY237+"      "+RED124+"      "+RED196+"                                 "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"      "+GRAY237+"      "+RED124+"      "+RED196+"                                 "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED88+"   "+GRAY237+"      "+RED124+"         "+RED196+"                              "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED88+"   "+GRAY237+"      "+RED124+"         "+RED196+"                           "+RED124+"   "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED88+"   "+GRAY237+"      "+RED124+"            "+RED196+"                        "+RED124+"   "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED88+"   "+GRAY237+"      "+RED124+"   "+RED88+"   "+RED124+"         "+RED196+"                  "+RED124+"      "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED124+"   "+RED88+"   "+GRAY237+"      "+RED124+"   "+RED88+"      "+RED124+"                        "+RED88+"   "+RED124+"   "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"   "+RED88+"      "+GRAY237+"      "+RED124+"      "+RED88+"      "+RED124+"                  "+RED88+"      "+RED124+"   "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"               "+GRAY237+"      "+RED88+"   "+GRAY237+"      "+RED124+"         "+RED88+"                           "+RED124+"   "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"                  "+GRAY237+"            "+RED124+"         "+RED88+"      "+GRAY237+"               "+RED88+"   "+RED124+"      "+GRAY237+"   "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED124+"            "+RED88+"   "+GRAY237+"   "+WHITE+"      "+GRAY237+"   "+RED88+"   "+RED124+"      "+GRAY237+"      "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED124+"            "+RED88+"   "+GRAY237+"   "+WHITE+"      "+GRAY237+"   "+RED88+"   "+RED124+"      "+GRAY237+"      "+WHITE+"                  "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED124+"         "+RED88+"      "+GRAY237+"   "+WHITE+"      "+GRAY237+"   "+RED88+"   "+RED124+"      "+GRAY237+"   "+WHITE+"                     "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"   "+RED124+"         "+RED88+"   "+GRAY237+"      "+WHITE+"      "+GRAY237+"               "+WHITE+"                     "+RESET);
+        System.out.println(WHITE+"                           "+GRAY237+"                  "+WHITE+"            "+GRAY237+"         "+WHITE+"                        "+RESET);
+        System.out.println(WHITE+"                              "+GRAY237+"            "+WHITE+"                                                "+RESET);
+        System.out.println(WHITE+"                                                                                          "+RESET);
+    }
+}
